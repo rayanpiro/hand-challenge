2f0651b6c779a6b4082529e44e17618446e3b028
rayanpiro <rubensangria@gmail.com>
2022-04-23 07:00:02 +0200

    Remove debugging and changing file to the good one

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod program_buffer;
 use program_buffer::Program;
 
 fn main() {
-    let program_path = "./test2.hand";
+    let program_path = "./input.hand";
     let app = Program::from_file(program_path);
-    dbg!(app.exec());
+    app.exec();
 }
